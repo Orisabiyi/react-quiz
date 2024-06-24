17362201284d19dd2a5f4c567d339a22eff58812
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-24 17:52:41 +0100

    created a questions component, disabled strict mode in development for special reason, created a reducer function added some state for status and questions

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,61 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "error":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "start" };
+    default:
+      return "Unknown";
+  }
+}
 
 function App() {
+  const [{ status, questions }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function getQuestions() {
+      try {
+        const response = await fetch(
+          `https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple`
+        );
+
+        if (!response.ok) throw new Error("Error Fetching questions");
+
+        const data = await response.json();
+
+        if (!data.results) return;
+
+        dispatch({ type: "dataReceived", payload: data.results });
+      } catch (error) {
+        dispatch({ type: "error" });
+      }
+    }
+
+    getQuestions();
+  }, []);
+
   return (
     <div className="app">
       <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen dispatch={dispatch} />}
+      </Main>
     </div>
   );
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,3 +1,16 @@
-export default function StartScreen() {
-  return;
+function StartScreen({ dispatch }) {
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quiz!</h2>
+      <h3>15 questions to test your react mastery</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's Start
+      </button>
+    </div>
+  );
 }
+
+export default StartScreen;
